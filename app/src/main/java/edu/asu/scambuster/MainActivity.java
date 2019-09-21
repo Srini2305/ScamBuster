32b187eaa75d89f3813aa13474bd2ca79d5f4032
Srinivasan Sundar <srini@srinivasans-mbp.3520.dhcp.asu.edu>
2019-09-21 02:11:34 -0700

    Initial project structure

---
+++ app/src/main/java/edu/asu/scambuster/MainActivity.java
@@ -0,0 +1,82 @@
+package edu.asu.scambuster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQ_CODE_SPEECH_INPUT = 100;
+    private TextView mVoiceInputTv;
+    private ImageButton mSpeakBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mVoiceInputTv = (TextView) findViewById(R.id.voiceInput);
+        mSpeakBtn = (ImageButton) findViewById(R.id.btnSpeak);
+        mSpeakBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startVoiceInput();
+            }
+        });
+    }
+
+    private void startVoiceInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hello, How can I help you?");
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        List<String> suspiciousWords = new ArrayList<>();
+        Boolean spam = false;
+        suspiciousWords.add("ssn");
+        suspiciousWords.add("bank account");
+        suspiciousWords.add("account number");
+        suspiciousWords.add("credit card");
+        suspiciousWords.add("debit card");
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    for (String word:result) {
+                        if(suspiciousWords.contains(word.toLowerCase())){
+                            spam = true;
+                        }
+                    }
+                    if(spam){
+                        mVoiceInputTv.setText("This could be a Suspicious call!!!");
+                    } else {
+                        mVoiceInputTv.setText(result.get(0));
+                    }
+                }
+                break;
+            }
+
+        }
+    }
+
+}
